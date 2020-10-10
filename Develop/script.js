b001ec0e90a71515fa0d415bf8434e5141ec84bc
Bert <robertgill82@gmail.com>
2020-10-10 12:22:40 -0700

    project reset

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,1 @@
 // Assignment code here
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-function generatePassword() {
-  return 0;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
