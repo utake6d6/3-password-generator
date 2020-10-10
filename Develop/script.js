9b7214bb2c5f466a4744648fd9b0e37e6be82536
Bert <robertgill82@gmail.com>
2020-10-10 13:01:12 -0700

    reset attempt_initial commit_chars listed begin pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,40 @@
 // Assignment code here
+// create variables for all elements to be manipulated by javascript
+var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#password");
+
+//create a function for clicking on the button
+generateBtn.addEventListener"click", function () {
+  //prompt to obtain the requested char length
+  var charLength = promt(
+    "How many characters would you like your new password to be? Please pick a number between 8 and 128."
+  );
+
+  //create a warning for 'if prompt' about inputing numbers being < or > set char range
+
+
+
+
+
+}
+
+//begin program if a character range is choosen, range must be between 8 and 128;
+if (charLength >= 8 && charLength <= 128) {
+  //create variables for password generater
+  let symbols = "!'#$%&()*+-./;:<>=?@[]{}~^";
+  let numbers = "0123456789";
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  //create a blank string for the new password generated;
+
+
+  //prompt user which type of characters they can choose from.
+  
+
+
+
+
+
+
+}
