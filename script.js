7430cbf499b8568562e11e68450f9f2890db2a03
Bert <robertgill82@gmail.com>
2020-10-11 12:42:23 -0700

    correcting html file location

--- script.js
+++ script.js
@@ -141,6 +141,7 @@ generateBtn.addEventListener("click", function () {
     ) {
       for (i = 0; i < charLength; i++) {
         let character = Math.floor(Math.random() * h.length);
+        newPassword += h.charAt(character, character + 1);
       }
     } else if (
       genSymbols === false &&
@@ -190,6 +191,7 @@ generateBtn.addEventListener("click", function () {
     ) {
       for (i = 0; i < charLength; i++) {
         let character = Math.floor(Math.random() * lowerCase.length);
+        newPassword += lowerCase.charAt(character, character + 1);
       }
     } else if (
       genSymbols === false &&
@@ -207,20 +209,3 @@ generateBtn.addEventListener("click", function () {
     passwordText.textContent = newPassword;
   }
 });
-
-//*create a password string that the computer can spit out
-//     let password = "";
-//     //create loop to run character generator 10 times
-//     for (var i = 0; i < length; i++) {
-//         var character = Math.floor(Math.random() * combind.length);
-//         password += combind.(character, character + 1);
-//     }
-//     console.log(password);
-
-//     function newPass() {
-//     //Set up arrays to hold all the random characters
-//     let symbols = "!'#$%&()*+-./;:<>=?@[]{}~^";
-//     let numbers = "0123456789";
-//     let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-//     let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     //combind all arrays to one variable(password)
